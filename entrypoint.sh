1483d6288563db0211b2157af11dd8a30d82d1c9
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-09-27 23:06:46 -0400

    feat: added version fetching functions.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 cd ./appdata
 
-JAR_FILE="fabric-server-launch.jar"
+JAR_FILE="server-fabric.jar"
 
 if [ ! -f "$JAR_FILE" ]; then
     echo "Jar file not found. Downloading..."
@@ -10,5 +10,24 @@ if [ ! -f "$JAR_FILE" ]; then
 else
     echo "Jar file already exists. Skipping download."
 fi
+
+
+get_latest_minecraft_version() {
+    curl -s https://meta.fabricmc.net/v2/versions/game | jq -r '[.[] | select(.stable == true)][0].version'
+}
+
+$MINECRAFT_VERSION = get_latest_minecraft_version()
+
+get_latest_loader_version(){
+    curl -s https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION | jq -r '[.[] | select(.loader.stable == true) | .loader.version][0]'
+}
+
+$LOADER_VERSION = get_latest_loader_version()
+
+$INSTALLER_VERSION = "1.0.1"
+
+download_server() {
+    curl https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION/$LOADER_VERSION/$INSTALLER_VERSION/server/jar
+}
 
 exec java -Xmx$JAVA_XMX -Xms$JAVA_XMS -jar $JAR_FILE nogui
