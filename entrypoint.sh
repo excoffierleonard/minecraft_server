36fe857e423d19583be3e9b888e05b77e42a4f44
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-09-27 23:15:47 -0400

    feat: should be working script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,11 @@
 
 cd ./appdata
 
-JAR_FILE="server-fabric.jar"
+SERVER_FILE_NAME="server-fabric.jar"
 
-if [ ! -f "$JAR_FILE" ]; then
+if [ ! -f "$SERVER_FILE_NAME" ]; then
     echo "Jar file not found. Downloading..."
-    curl -o "$JAR_FILE" https://meta.fabricmc.net/v2/versions/loader/1.21.1/0.16.5/1.0.1/server/jar
+    download_server()
 else
     echo "Jar file already exists. Skipping download."
 fi
@@ -16,18 +16,16 @@ get_latest_minecraft_version() {
     curl -s https://meta.fabricmc.net/v2/versions/game | jq -r '[.[] | select(.stable == true)][0].version'
 }
 
-$MINECRAFT_VERSION = get_latest_minecraft_version()
-
 get_latest_loader_version(){
     curl -s https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION | jq -r '[.[] | select(.loader.stable == true) | .loader.version][0]'
 }
 
+$MINECRAFT_VERSION = get_latest_minecraft_version()
 $LOADER_VERSION = get_latest_loader_version()
-
 $INSTALLER_VERSION = "1.0.1"
 
 download_server() {
-    curl https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION/$LOADER_VERSION/$INSTALLER_VERSION/server/jar
+    curl -o $SERVER_FILE_NAME https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION/$LOADER_VERSION/$INSTALLER_VERSION/server/jar
 }
 
 exec java -Xmx$JAVA_XMX -Xms$JAVA_XMS -jar $JAR_FILE nogui
