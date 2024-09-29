e1173344d922e0b08af28f39cb3f28980701cf04
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-09-29 18:24:46 -0400

    feat: added some more verbose about the current envs used.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,36 @@
 #!/bin/bash
 
+# Exit if some environment variables are not set
+: ${JAVA_XMS:?"Environment variable JAVA_XMS is required but not set"}
+: ${JAVA_XMX:?"Environment variable JAVA_XMX is required but not set"}
+: ${MINECRAFT_VERSION:?"Environment variable MINECRAFT_VERSION is required but not set"}
+
+# Current Environment Variables
+echo "JAVA_XMS: $JAVA_XMS"
+echo "JAVA_XMX: $JAVA_XMX"
+echo "MINECRAFT_VERSION: $MINECRAFT_VERSION"
+
 cd ./appdata
 
 SERVER_FILE_PATTERN="server-*.jar"
 
+# Get the latest Minecraft version
 get_latest_minecraft_version() {
     curl -s https://meta.fabricmc.net/v2/versions/game | jq -r '[.[] | select(.stable == true)][0].version'
 }
 
+# Get the latest loader version for the specified Minecraft version
 get_latest_loader_version() {
     curl -s https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION | jq -r '[.[] | select(.loader.stable == true) | .loader.version][0]'
 }
 
+# Download the server file
 download_server() {
-    # Check if MINECRAFT_VERSION is set to "latest"
     if [ "$MINECRAFT_VERSION" = "latest" ]; then
         MINECRAFT_VERSION=$(get_latest_minecraft_version)
-        echo "Using latest Minecraft version: $MINECRAFT_VERSION"
-    elif [ -z "$MINECRAFT_VERSION" ]; then
-        echo "MINECRAFT_VERSION environment variable is not set. Please set it to a version number or 'latest'."
-        exit 1
     fi
+
+    echo "Downloading Minecraft version: $MINECRAFT_VERSION"
 
     LOADER_VERSION=$(get_latest_loader_version)
     INSTALLER_VERSION="1.0.1"
@@ -29,6 +39,7 @@ download_server() {
     curl -o $SERVER_FILE_NAME https://meta.fabricmc.net/v2/versions/loader/$MINECRAFT_VERSION/$LOADER_VERSION/$INSTALLER_VERSION/server/jar
 }
 
+# Check if the server file exists
 if [ ! -f $SERVER_FILE_PATTERN ]; then
     echo "Server file not found. Downloading..."
     download_server
@@ -37,4 +48,5 @@ else
     SERVER_FILE_NAME=$(ls $SERVER_FILE_PATTERN | head -n 1)
 fi
 
+# Start the server
 exec java -Xms$JAVA_XMS -Xmx$JAVA_XMX -jar $SERVER_FILE_NAME nogui
