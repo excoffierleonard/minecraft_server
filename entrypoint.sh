72deac5e2a10c1675e4990e3d76a293541bafae8
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-09-29 16:37:50 -0400

    exec: changed permission of entrypoint.sh script so it can be run

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ download_server() {
 }
 
 if [ ! -f $SERVER_FILE_PATTERN ]; then
-    echo "Server file not found. Downloading.S."
+    echo "Server file not found. Downloading..."
     download_server
 else
     echo "Server file already exists. Skipping download."
